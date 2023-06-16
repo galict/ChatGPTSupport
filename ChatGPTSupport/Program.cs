060f826e938e8e2ffadae3544fc3e34cd6aff217
Tomislav Galić <67128743+galict@users.noreply.github.com>
2023-06-16 10:06:08 +0200

    Initial commit

---
+++ ChatGPTSupport/Program.cs
@@ -0,0 +1,34 @@
+using OpenAI.Net;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddRazorPages();
+builder.Services.AddServerSideBlazor();
+
+//Add OpenAIServices
+builder.Services.AddOpenAIServices(o =>
+{
+    o.ApiKey = builder.Configuration["OpenAI:ApiKey"]; //ChatGPT API key value from secrets.json
+});
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Error");
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.MapBlazorHub();
+app.MapFallbackToPage("/_Host");
+
+app.Run();
